1113fff2906f1e0492316510a3b1d59403d68000
Lee Sharma <lee@leesharma.com>
2020-10-11 18:22:10 -0400

    Add demo model file
    Once again, to demonstrate how to use multiple files. All content from
      https://reservoir.sciml.ai/dev/examples/esn/

--- main.jl
+++ main.jl
@@ -7,9 +7,15 @@ using DataFrames    # for easy data formatting and statistics
 include("./data/lorenz.jl")
 using .LorenzData
 
+include("./models/base_esn.jl")
+using .BaseESN
+
 
 function main()
-  train, test = LorenzData.train_test(train_len=8, predict_len=5)
+
+  # Data Loading
+
+  train, test = LorenzData.train_test(train_len=3000, predict_len=5)
 
   println("Lorenz System")
   println("-------------")
@@ -18,11 +24,23 @@ function main()
   println("  test:  $(size(test))")
   println()
   println("Training data:")
-  display(DataFrame(train', ["x","y","z"]))
+  display(first(DataFrame(train', ["x","y","z"]), 8))
   println()
   println("Testing data:")
   display(DataFrame(test', ["x","y","z"]))
   println()
+  println()
+
+  # Run the Model
+
+  esn = BaseESN.esn(train)
+  W_out = BaseESN.train(esn)
+  output = BaseESN.predict(esn, 5, W_out)
+
+  println("Predictions:")
+  display(DataFrame(output', ["x","y","z"]))
+  println()
+
 end
 
 
---
+++ models/base_esn.jl
@@ -0,0 +1,39 @@
+module BaseESN
+  export esn, train, predict
+
+  using ReservoirComputing
+
+  # default model parameters
+  approx_res_size = 300   # size of the reservoir
+  radius = 1.2            # desired spectral radius
+  activation = tanh       # neuron activation function
+  degree = 6              # degree of connectivity of the reservoir
+  sigma = 0.1             # input weight scaling
+  beta = 0.0              # ridge
+  alpha = 1.0             # leaky coefficient
+  nla_type = NLAT2()      # non linear algorithm for the states
+  extended_states = false # if true extends the states with the input
+
+  # public interface
+
+  function esn(data)
+    """Create echo state network"""
+    ESN(approx_res_size,
+      data,
+      degree,
+      radius,
+      activation = activation,
+      sigma = sigma,
+      alpha = alpha,
+      nla_type = nla_type,
+      extended_states = extended_states)
+  end
+
+  function train(esn)
+    W_out = ESNtrain(esn, beta)
+  end
+
+  function predict(esn, predict_len, W_out)
+    output = ESNpredict(esn, predict_len, W_out)
+  end
+end
