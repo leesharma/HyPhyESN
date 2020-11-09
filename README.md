d1eac22d06f98b8858327e57138ca649c899d9d0
mrziema2 <66431227+mrziema2@users.noreply.github.com>
2020-11-09 17:22:52 -0500

    Implemented JGCM Primitives Solutions
    Added a primitive equations solver with three implementations. To be used for numerical weather forecast work. See README section Climate Forecast Models for details.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # HyPhyESN
 
 Class project for CMSC 727
@@ -61,3 +62,116 @@ You should be able to run this script without errors:
 ```bash
 % julia main.jl
 ```
+## Climate Forecast Models
+
+The `./data` directory contains three variants of simplified climate forecast models with varying degrees of complexity.
+These codes utilize the classical spectral atmosphere model developed in the NOAA Geophysical Fluid Dynamics Laboratory,
+and implementations are provided by [JGCM](https://github.com/CliMA/IdealizedSpectralGCM.jl).
+
+These spectral methods solve the primitive equations before any physical parameterization. This "spectral core" is used in
+ALL numerical weather forecast models (albeit with some slight modifications). Parameterizations are then applied afterwards
+to correct the solutions to sub-grid phenomena.
+
+NOTE: I am still rather unclear on the details, and anything I say has a high chance of being incorrect.
+
+Further details on these specific models can be found [here](https://www.gfdl.noaa.gov/idealized-spectral-models-quickstart/).
+
+In short, `barotropic.jl` is the simplest model, implementing only vorticity equations for the evolution of a 2D non-divergent
+flow on the surface of a sphere.
+
+`shallow_water.jl` builds on this by adding in the equation for hydrostatic balance, to the approximation that the ratio of vertical scale
+to horizontal scale is small.
+
+`spectral_dynamics.jl` contains the full spectral method, solving all 6 primitive equations. It is very slow. I've modified it to easily
+change the timescales and grid system.
+
+All three of these output a simple plot to `./data/data_plots`. This output can be modified to suit our needs moving forward.
+=======
+# HyPhyESN
+
+Class project for CMSC 727
+
+## Getting Started
+
+### Project Dependencies
+
+* Julia 1.5 ([install link](https://julialang.org/downloads/))
+
+### In the REPL
+
+You can use the interactive Julia REPL either through your terminal or in Juno.
+To experiment in the REPL, you need to first activate the project environment:
+
+```bash
+% cd $(project_root)                # move to the HyPhyESN root directory
+% julia                             # start the Julia REPL
+...
+
+julia> ]                            # open the package manager context
+
+(@v1.5) pkg>  activate .            # activate our Project.toml
+(HyPhyESN) pkg> instantiate         # instantiate the project (adding deps, etc.)
+(HyPhyESN) pkg> st                  # print project status (should show deps)
+  [1dea7af3] OrdinaryDiffEq v5.43.0
+  [65888b18] ParameterizedFunctions v5.6.0
+  [7c2d2b1e] ReservoirComputing v0.6.1
+
+
+# optional, will save start-up time later
+(HyPhyESN) pkg> precompile
+Precompiling HyPhyESN...
+...
+```
+
+The `activate` step must be performed for every new Julia context (e.g. whenever you restart the REPL).
+The `instantiate` and `precompile` steps only need to be performed once.
+
+### In a File
+
+Running a Julia file creates a new Julia context, so we need to explicitly activate our project environment.
+You can do this by adding the following line to the head of your main `.jl` file:
+
+```julia
+using Pkg; Pkg.activate("."); Pkg.instantiate()
+```
+
+If you plan to run the file from a different folder, replace `"."` with the path to `Project.toml`'s parent folder.
+
+### In Juno
+
+You can set up the Juno console using the steps in ["In the REPL"](#in-the-repl) above.
+
+If you plan to run from a file, use the steps in ["In a File"](#in-a-file) above.
+
+### Verify Setup
+
+You should be able to run this script without errors:
+
+```bash
+% julia main.jl
+```
+## Climate Forecast Models
+
+The `./data` directory contains three variants of simplified climate forecast models with varying degrees of complexity.
+These codes utilize the classical spectral atmosphere model developed in the NOAA Geophysical Fluid Dynamics Laboratory,
+and implementations are provided by [JGCM](https://github.com/CliMA/IdealizedSpectralGCM.jl).
+
+These spectral methods solve the primitive equations before any physical parameterization. This "spectral core" is used in
+ALL numerical weather forecast models (albeit with some slight modifications). Parameterizations are then applied afterwards
+to correct the solutions to sub-grid phenomena.
+
+NOTE: I am still rather unclear on the details, and anything I say has a high chance of being incorrect.
+
+Further details on these specific models can be found [here](https://www.gfdl.noaa.gov/idealized-spectral-models-quickstart/).
+
+In short, `barotropic.jl` is the simplest model, implementing only vorticity equations for the evolution of a 2D non-divergent
+flow on the surface of a sphere.
+
+`shallow_water.jl` builds on this by adding in the equation for hydrostatic balance, to the approximation that the ratio of vertical scale
+to horizontal scale is small.
+
+`spectral_dynamics.jl` contains the full spectral method, solving all 6 primitive equations. It is very slow. I've modified it to easily
+change the timescales and grid system.
+
+All three of these output a simple plot to `./data/data_plots`. This output can be modified to suit our needs moving forward.
+>>>>>>> Stashed changes
---
+++ data/spectral_dynamics.jl
@@ -0,0 +1,257 @@
+<<<<<<< Updated upstream
+using JGCM
+
+function Run_Spectral_Dynamics()
+    #include("HS.jl")  # For adding in parameterizations.
+
+    #############################################################
+    #-- Define spectral (Gaussian) grid resolution
+    grid_name = "T21"
+
+    # T21 grid
+    num_fourier = 21  # Fourier wave number truncation
+    nθ = 32  # Latitudinal grid size
+    nd = 20  # Vertical slices
+
+    # T42 grid
+    #num_fourier = 42  # Fourier wave number truncation
+    #nθ = 64  # Latitudinal grid size
+    #nd = 20  # Vertical slices
+
+    # CFS standard spectral grid resolution, T126L64
+    #num_fourier = 126
+    #nθ = 190
+    #nd = 64
+
+    #-- Define temporal parameters
+    end_day = 2 # Default 1200
+    spinup_day = 1 # Default 200; days to let model reach equilibrium before pulling data
+
+    physics_params = Dict{String,Float64}("σ_b"=>0.7, "k_f" => 1.0, "k_a" => 1.0/40.0, "k_s" => 1.0/4.0, "ΔT_y" => 60.0, "Δθ_z" => 10.0)
+    op_man = Atmos_Spectral_Dynamics_Main(physics_params, num_fourier, nθ, nd, end_day, spinup_day)
+    #Finalize_Output!(op_man, "HS_OpM.dat", "HS_mean.dat")  # To implement parameterizations
+    Sigma_Zonal_Mean_Contourf(op_man, "./data/data_plots/spectral_dynamics_contourf_$grid_name")
+end
+
+function Atmos_Spectral_Dynamics_Main(physics_params::Dict{String, Float64}, num_fourier::Int64 = 42, nθ::Int64 = 64, nd::Int64 = 20, end_day::Int64 = 1200, spinup_day::Int64 = 200)
+  # the decay of a sinusoidal disturbance to a zonally symmetric flow
+  # that resembles that found in the upper troposphere in Northern winter. (this description might be wrong?)
+  name = "Spectral_Dynamics"
+  #num_fourier, nθ, nd = 42, 64, 20  # T42 defaults
+  #num_fourier, nθ, nd = 21, 32, 20  # T21 defaults
+
+  num_spherical = num_fourier + 1
+  nλ = 2nθ
+
+  radius = 6371000.0
+  omega = 7.292e-5
+  sea_level_ps_ref = 1.0e5
+  init_t = 264.0
+
+
+  # Initialize mesh
+  mesh = Spectral_Spherical_Mesh(num_fourier, num_spherical, nθ, nλ, nd, radius)
+  θc, λc = mesh.θc,  mesh.λc
+  cosθ, sinθ = mesh.cosθ, mesh.sinθ
+
+  vert_coord = Vert_Coordinate(nλ, nθ, nd, "even_sigma", "simmons_and_burridge", "second_centered_wts", sea_level_ps_ref)
+  # Initialize atmo_data
+  do_mass_correction = true
+  do_energy_correction = true
+  do_water_correction = false
+
+  use_virtual_temperature = false
+  atmo_data = Atmo_Data(name, nλ, nθ, nd, do_mass_correction, do_energy_correction, do_water_correction, use_virtual_temperature, sinθ, radius,  omega)
+
+  # Initialize integrator
+  damping_order = 4
+  damping_coef = 1.15741e-4
+  robert_coef  = 0.04
+
+  implicit_coef = 0.5
+  day_to_sec = 86400
+  start_time = 0
+  end_time = end_day*day_to_sec  #
+  Δt = 1200
+  init_step = true
+
+  integrator = Filtered_Leapfrog(robert_coef,
+  damping_order, damping_coef, mesh.laplacian_eig,
+  implicit_coef, Δt, init_step, start_time, end_time)
+
+  ps_ref = sea_level_ps_ref
+  t_ref = fill(300.0, nd)
+  wave_numbers = mesh.wave_numbers
+  semi_implicit = Semi_Implicit_Solver(vert_coord, atmo_data,
+  integrator, ps_ref, t_ref, wave_numbers)
+
+
+  # Data Visualization
+  op_man= Output_Manager(mesh, vert_coord, start_time, end_time, spinup_day)
+
+
+  # Initialize data
+  dyn_data = Dyn_Data(name, num_fourier, num_spherical, nλ, nθ, nd)
+
+
+  NT = Int64(end_time / Δt)
+
+  Get_Topography!(dyn_data.grid_geopots)
+
+  Spectral_Initialize_Fields!(mesh, atmo_data, vert_coord, sea_level_ps_ref, init_t,
+  dyn_data.grid_geopots, dyn_data)
+
+
+  Atmosphere_Update!(mesh, atmo_data, vert_coord, semi_implicit, dyn_data, physics_params)
+  Update_Init_Step!(semi_implicit)
+  integrator.time += Δt
+  Update_Output!(op_man, dyn_data, integrator.time)
+
+  for i = 2:NT
+
+    Atmosphere_Update!(mesh, atmo_data, vert_coord, semi_implicit, dyn_data, physics_params)
+
+    integrator.time += Δt
+    #@info integrator.time
+
+    Update_Output!(op_man, dyn_data, integrator.time)
+
+    if (integrator.time%day_to_sec == 0)
+      @info "Day: ", div(integrator.time,day_to_sec), " Max |U|,|V|,|P|,|T| : ", maximum(abs.(dyn_data.grid_u_c)), maximum(abs.(dyn_data.grid_v_c)), maximum(dyn_data.grid_p_full), maximum(dyn_data.grid_t_c)
+    end
+
+
+  end
+
+  return op_man
+
+end
+=======
+using JGCM
+
+function Run_Spectral_Dynamics()
+    #include("HS.jl")  # For adding in parameterizations.
+
+    #############################################################
+    #-- Define spectral (Gaussian) grid resolution
+    grid_name = "T21"
+
+    # T21 grid
+    num_fourier = 21  # Fourier wave number truncation
+    nθ = 32  # Latitudinal grid size
+    nd = 20  # Vertical slices
+
+    # T42 grid
+    #num_fourier = 42  # Fourier wave number truncation
+    #nθ = 64  # Latitudinal grid size
+    #nd = 20  # Vertical slices
+
+    # CFS standard spectral grid resolution, T126L64
+    #num_fourier = 126
+    #nθ = 190
+    #nd = 64
+
+    #-- Define temporal parameters
+    end_day = 2 # Default 1200
+    spinup_day = 1 # Default 200; days to let model reach equilibrium before pulling data
+
+    physics_params = Dict{String,Float64}("σ_b"=>0.7, "k_f" => 1.0, "k_a" => 1.0/40.0, "k_s" => 1.0/4.0, "ΔT_y" => 60.0, "Δθ_z" => 10.0)
+    op_man = Atmos_Spectral_Dynamics_Main(physics_params, num_fourier, nθ, nd, end_day, spinup_day)
+    #Finalize_Output!(op_man, "HS_OpM.dat", "HS_mean.dat")  # To implement parameterizations
+    Sigma_Zonal_Mean_Contourf(op_man, "./data/data_plots/spectral_dynamics_contourf_$grid_name")
+end
+
+function Atmos_Spectral_Dynamics_Main(physics_params::Dict{String, Float64}, num_fourier::Int64 = 42, nθ::Int64 = 64, nd::Int64 = 20, end_day::Int64 = 1200, spinup_day::Int64 = 200)
+  # the decay of a sinusoidal disturbance to a zonally symmetric flow
+  # that resembles that found in the upper troposphere in Northern winter. (this description might be wrong?)
+  name = "Spectral_Dynamics"
+  #num_fourier, nθ, nd = 42, 64, 20  # T42 defaults
+  #num_fourier, nθ, nd = 21, 32, 20  # T21 defaults
+
+  num_spherical = num_fourier + 1
+  nλ = 2nθ
+
+  radius = 6371000.0
+  omega = 7.292e-5
+  sea_level_ps_ref = 1.0e5
+  init_t = 264.0
+
+
+  # Initialize mesh
+  mesh = Spectral_Spherical_Mesh(num_fourier, num_spherical, nθ, nλ, nd, radius)
+  θc, λc = mesh.θc,  mesh.λc
+  cosθ, sinθ = mesh.cosθ, mesh.sinθ
+
+  vert_coord = Vert_Coordinate(nλ, nθ, nd, "even_sigma", "simmons_and_burridge", "second_centered_wts", sea_level_ps_ref)
+  # Initialize atmo_data
+  do_mass_correction = true
+  do_energy_correction = true
+  do_water_correction = false
+
+  use_virtual_temperature = false
+  atmo_data = Atmo_Data(name, nλ, nθ, nd, do_mass_correction, do_energy_correction, do_water_correction, use_virtual_temperature, sinθ, radius,  omega)
+
+  # Initialize integrator
+  damping_order = 4
+  damping_coef = 1.15741e-4
+  robert_coef  = 0.04
+
+  implicit_coef = 0.5
+  day_to_sec = 86400
+  start_time = 0
+  end_time = end_day*day_to_sec  #
+  Δt = 1200
+  init_step = true
+
+  integrator = Filtered_Leapfrog(robert_coef,
+  damping_order, damping_coef, mesh.laplacian_eig,
+  implicit_coef, Δt, init_step, start_time, end_time)
+
+  ps_ref = sea_level_ps_ref
+  t_ref = fill(300.0, nd)
+  wave_numbers = mesh.wave_numbers
+  semi_implicit = Semi_Implicit_Solver(vert_coord, atmo_data,
+  integrator, ps_ref, t_ref, wave_numbers)
+
+
+  # Data Visualization
+  op_man= Output_Manager(mesh, vert_coord, start_time, end_time, spinup_day)
+
+
+  # Initialize data
+  dyn_data = Dyn_Data(name, num_fourier, num_spherical, nλ, nθ, nd)
+
+
+  NT = Int64(end_time / Δt)
+
+  Get_Topography!(dyn_data.grid_geopots)
+
+  Spectral_Initialize_Fields!(mesh, atmo_data, vert_coord, sea_level_ps_ref, init_t,
+  dyn_data.grid_geopots, dyn_data)
+
+
+  Atmosphere_Update!(mesh, atmo_data, vert_coord, semi_implicit, dyn_data, physics_params)
+  Update_Init_Step!(semi_implicit)
+  integrator.time += Δt
+  Update_Output!(op_man, dyn_data, integrator.time)
+
+  for i = 2:NT
+
+    Atmosphere_Update!(mesh, atmo_data, vert_coord, semi_implicit, dyn_data, physics_params)
+
+    integrator.time += Δt
+    #@info integrator.time
+
+    Update_Output!(op_man, dyn_data, integrator.time)
+
+    if (integrator.time%day_to_sec == 0)
+      @info "Day: ", div(integrator.time,day_to_sec), " Max |U|,|V|,|P|,|T| : ", maximum(abs.(dyn_data.grid_u_c)), maximum(abs.(dyn_data.grid_v_c)), maximum(dyn_data.grid_p_full), maximum(dyn_data.grid_t_c)
+    end
+
+
+  end
+
+  return op_man
+
+end
+>>>>>>> Stashed changes
