e4dab7ec8266c0e4ebdb3eae750acd7117fa2873
Matt Ziemann <mrziema2@gmail.com>
2021-02-03 11:11:11 -0500

    Various HPC fixes

---
+++ PBS/PBS_spectral_baseESN_job1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#PBS -l walltime=8:00:00
+#PBS -l select=1:ncpus=40:mpiprocs=40
+#PBS -l place=scatter:excl
+#PBS -N spectral_baseESN_job1
+#PBS -q standard
+##PBS -r y
+#PBS -j oe
+#PBS -A ARLAP00581800
+#
+#
+echo job ${JOBID} starting at `date` on `hostname`
+echo starting in `pwd`
+#
+#
+cd /p/home/mrziema/projects/HyPhyESN
+julia train/spectral_base_job1.jl
+
+set st=$status
+echo "Program ended with status $st on `date`" 
+
+exit $st
---
+++ PBS/PBS_spectral_baseESN_job3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#PBS -l walltime=6:00:00
+#PBS -l select=1:ncpus=40:mpiprocs=40
+#PBS -l place=scatter:excl
+#PBS -N spectral_baseESN_job3
+#PBS -q standard
+##PBS -r y
+#PBS -j oe
+#PBS -A ARLAP00581800
+#
+#
+echo job ${JOBID} starting at `date` on `hostname`
+echo starting in `pwd`
+#
+#
+cd /p/home/mrziema/projects/HyPhyESN
+julia train/spectral_base_job3.jl
+
+set st=$status
+echo "Program ended with status $st on `date`" 
+
+exit $st
